5f022fc89088e60425017a98686a89ea7756bcf3
luciano-mota <amoosenhorjesus3@gmail.com>
2024-08-22 16:16:05 -0300

    Created profiles

--- src/main/kotlin/com/learningkotlin/avengers/application/web/resource/AvengerResource.kt
+++ src/main/kotlin/com/learningkotlin/avengers/application/web/resource/AvengerResource.kt
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.servlet.mvc.method.annotation.ServletResponseMethodArgumentResolver
 import java.net.URI
 
 private const val API_PATH = "/v1/api/avenger"
